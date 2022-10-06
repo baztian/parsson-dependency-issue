c49e15c526abb3289b3dfe3ecd1497196f5e4a53
baztian <bastian.dev@gmail.com>
2022-10-06 17:05:03 +0200

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import static jakarta.json.Json.createObjectBuilder;
+
+import jakarta.json.JsonObject;
+
+class App {
+
+    public static void main(String[] args) {
+        JsonObject deliveryEvent = createObjectBuilder()
+            .add("mail", createObjectBuilder()
+                .add("timestamp", "2021-07-09T09:02:45.234386+00:00")
+                .add("messageId", "3191054b-ef87-4959-81af-9e851088a507")
+                .build()
+            ).build();
+    }
+}
+
